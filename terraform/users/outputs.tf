f34074566108f71141f5b9d75c4f0408f2ccbfe1
sptree-m <52631857+sptree-m@users.noreply.github.com>
2026-09-12 14:23:08 +0900

    Expose per-user IAM role in Terraform output

--- terraform/users/outputs.tf
+++ terraform/users/outputs.tf
@@ -1,15 +1,16 @@
 output "users" {
-  description = "Resolved per-user lifecycle state and EFS Access Point IDs."
+  description = "Resolved per-user lifecycle state and EFS/IAM resources."
   value = {
     for username, user in local.users :
     username => {
-      uid             = user.uid
-      gid             = user.gid
-      enabled         = user.enabled
-      bedrock_tag     = user.bedrock_tag
+      uid              = user.uid
+      gid              = user.gid
+      enabled          = user.enabled
+      bedrock_tag      = user.bedrock_tag
       efs_access_point = aws_efs_access_point.user[username].id
-      home_path       = "${var.home_root}/${username}"
-      container_root  = "${var.local_container_root}/${username}"
+      bedrock_role_arn = try(aws_iam_role.bedrock_user[username].arn, null)
+      home_path        = "${var.home_root}/${username}"
+      container_root   = "${var.local_container_root}/${username}"
     }
   }
 }
